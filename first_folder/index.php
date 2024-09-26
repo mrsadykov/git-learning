7a385696fe91415bba28c038aa591e2ac460656e
Iskandar Sadykov <iskandar.sadykov@simbirsoft.com>
2024-09-26 12:35:28 +0500

    dev-3, commit 12:25

--- first_folder/index.php
+++ first_folder/index.php
@@ -4,5 +4,6 @@ echo 'branch dev-1, /first_folder/index.php';
 echo 'branch dev-1, new commit';
 echo 'branch dev-1, commit 12:28';
 echo 'branch dev-1, commit 12:34';
+echo 'branch dev-3, commit 12:35';
 
 ?>
